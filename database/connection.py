4640ccede37f63baf8a880d65c5434fe7979c7fc
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-10-10 22:00:39 -0300

    create models

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,14 @@
-
 from os import getenv
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL=getenv('DATABASE_URL')
-
+DATABASE_URL = getenv('DATABASE_URL')
 
 engine = create_async_engine(DATABASE_URL)
-async_session = sessionmaker(engine,class_=AsyncSession)
+
+# Crie uma sessão AsyncSession
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession)
+
+# Para criar uma instância de sessão, use AsyncSessionLocal()
+async_session = AsyncSessionLocal()
