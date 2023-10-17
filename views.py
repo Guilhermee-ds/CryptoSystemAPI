19e2d276e2b25251383db34ba25c2e592992ee21
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-10-17 12:13:53 -0300

    Update Views

--- views.py
+++ views.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
-from services import UserService  
+from services import UserService 
+from schemas import UserCreateInput
 
 
 user_router = APIRouter(perfix='/user')
 assets_router = APIRouter(prefix='/assets')
 
 @user_router.post('/create')
-async def user_create():
+async def user_create(user_input: UserCreateInput):
     try:
-       await UserService.create_user(name='name..')
+       await UserService.create_user(name=user_input.name)
     except:
         pass
