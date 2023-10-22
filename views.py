3da866df677e40d04b1b435ba045f5a71011d2e8
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-10-22 11:19:02 -0300

    input first user from database use api

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from services import UserService
 from schemas import UserCreateInput, StandardOutput, ErrorOutput
 
@@ -11,5 +11,5 @@ async def user_create(user_input: UserCreateInput):
     try:
        await UserService.create_user(name=user_input.name)
        return StandardOutput(message="OK")
-    except:
-        pass
+    except Exception as error:
+        raise HTTPException(400, detail= str(error))
