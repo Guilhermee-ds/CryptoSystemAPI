e10817e13934fc5c98c0a9b7988f2660729ed68d
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-10-23 12:40:39 -0300

    create first commit for delete user

--- services.py
+++ services.py
@@ -1,4 +1,5 @@
 
+from sqlalchemy import delete
 from database.models import User
 from database.connection import async_session
 
@@ -8,3 +9,7 @@ class UserService:
         async with async_session() as session:
             session.add(User(name=name))
             await session.commit()
+async def delete_user(user_id):
+    async with async_session() as session:
+        await session.execute(delete(User).where(User.id==user_id))
+        await session.commit()
