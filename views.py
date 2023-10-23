d25c04fd57f12a5b5c5c20391bd077734c39bf91
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-10-23 13:17:26 -0300

    create add favorite coin from database

--- views.py
+++ views.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, HTTPException
-from services import UserService
-from schemas import UserCreateInput, StandardOutput, ErrorOutput
+from services import UserService, FavoriteService
+
+from schemas import (UserCreateInput, 
+                     StandardOutput,
+                     ErrorOutput,
+                     UserFavoriteAddInput
+                     )
 
 
 user_router = APIRouter(prefix='/user')
@@ -15,10 +20,17 @@ async def user_create(user_input: UserCreateInput):
         raise HTTPException(400, detail= str(error))
     
 @user_router.delete('/delete/{user_id}',response_model=StandardOutput, responses={400:{'model':ErrorOutput}})
-async def user_create(user_id:int):
+async def user_delete(user_id:int):
     try:
        await UserService.delete_user(user_id)
        return StandardOutput(message="OK")
     except Exception as error:
         raise HTTPException(400, detail= str(error))
-    
+
+@user_router.post('/favorite/add',response_model=StandardOutput, responses={400:{'model':ErrorOutput}})
+async def user_favorite_add(favorite_add: UserFavoriteAddInput):
+    try:
+       await FavoriteService.add_favorite(user_id=favorite_add.user_id, symbol=favorite_add.symbol)
+       return StandardOutput(message="OK")
+    except Exception as error:
+        raise HTTPException(400, detail= str(error))
